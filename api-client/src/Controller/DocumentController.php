765a10dc11b10988f188a943f541c0738052ac86
Samuel Belolo <sbelolo@makheia.com>
2022-03-08 01:03:48 +0100

    :sparkles: added read document request

--- api-client/src/Controller/DocumentController.php
+++ api-client/src/Controller/DocumentController.php
@@ -10,13 +10,15 @@ use App\Entity\OpenApiDocument;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Utils\Validator;
+use App\Repository\OpenApiDocumentRepository;
+use Ramsey\Uuid\Uuid;
 
 #[Route('/document')]
 class DocumentController extends AbstractController
 {
 
-    #[Route('', name: 'document_post', methods:['POST'])]
-    public function index(Request $request, ManagerRegistry $doctrine, Validator $validator): Response
+    #[Route('', name: 'document_create', methods: ['POST'])]
+    public function create(Request $request, ManagerRegistry $doctrine, Validator $validator): Response
     {
         $data = json_decode($request->getContent(), true) ?? [];
 
@@ -40,6 +42,21 @@ class DocumentController extends AbstractController
         return new JsonResponse([
             'sucess'    => true,
             'data'      => $document->getId()
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/{id}', name: 'document_read')]
+    public function read(string $id, OpenApiDocumentRepository $repo){
+
+        if (!Uuid::isValid($id)) return new JsonResponse([ 'sucess' => false ], Response::HTTP_UNAUTHORIZED);
+
+        $document = $repo->find($id);
+
+        if (!$document) return new JsonResponse([ 'sucess' => false ], Response::HTTP_NOT_FOUND);
+
+        return new JsonResponse([
+            'sucess'    => true,
+            'data'      => $document->toArray()
         ], Response::HTTP_OK);
     }
 }
