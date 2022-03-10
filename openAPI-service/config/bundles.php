5b252bc559bccc416a3522773bb8575ab5d37e8f
PIERRON Marc <pierronmarc@outlook.fr>
2022-03-10 21:00:36 +0100

    :package: Add dependencies

--- openAPI-service/config/bundles.php
+++ openAPI-service/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
