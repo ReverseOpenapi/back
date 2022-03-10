f22ce8b02ca2a6d41106ec9bc452fe2f99ef5eab
Samuel Belolo <sbelolo@makheia.com>
2022-03-10 19:38:08 +0100

    :package: added sentry/sentry-symfony

--- api-client/config/bundles.php
+++ api-client/config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
