407a683b85ddefc999adf40fd5b113c5bf265969
Samuel Belolo <sbelolo@makheia.com>
2022-03-10 19:43:29 +0100

    :tada: init

---
+++ api-client/src/Controller/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response};
+use Ramsey\Uuid\Uuid;
+use OpenApi\Attributes as OA;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+
+
+
+#[Route('/api/test')]
+#[OA\Tag(name: "Test")]
+class TestController extends AbstractController
+{
+
+    #[Route('/download/{id}', name:'document_test', methods: ['GET'])]
+    public function retrieve(FileSystem $fs, string $id) {
+
+        if (!Uuid::isValid($id)) return new JsonResponse([ 'success' => false ], Response::HTTP_UNAUTHORIZED);
+
+        $filename = $id . '.json';
+
+        if (!$fs->has($filename)) return new JsonResponse([ 'success' => false ], Response::HTTP_NOT_FOUND);
+
+        $file = $fs->get($filename);
+        $response = new Response($file->getContent());
+
+        $disposition = HeaderUtils::makeDisposition(
+            HeaderUtils::DISPOSITION_ATTACHMENT,
+            $filename
+        );
+        
+        $response->headers->set('Content-Disposition', $disposition);
+        return $response;
+    }
+
+}
